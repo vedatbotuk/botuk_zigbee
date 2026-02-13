d98367f3eec65c00258e95bc88df29dfd4c17c51
Vedat <vedat.botuk@gmail.com>
2026-02-13 11:08:10 +0100

    deinit not possible, update noel star

---
+++ homeassistant-zigbee2mqtt/ESP32H2_Sensor_58432.js
@@ -0,0 +1,18 @@
+import * as m from 'zigbee-herdsman-converters/lib/modernExtend';
+
+export default {
+    zigbeeModel: ['58432'],
+    model: '58432',
+    vendor: 'Botuk',
+    description: 'Noel Star with temperature and humidity sensor',
+    extend: [
+        m.humidity({
+            reporting: {min: 10, max: 3600, change: 100} // min 10s, max 1h, change 1.0%
+        }), 
+        m.temperature({
+            reporting: {min: 10, max: 3600, change: 10}  // min 10s, max 1h, change 0.1°C
+        }), 
+        m.onOff({"powerOnBehavior": false})
+    ],
+    ota: true
+};
